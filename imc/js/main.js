43ca136987fce41ae78e54149e59bbdf7485b943
Rz <ramon.o.azevedo@aluno.senai.br>
2023-12-05 18:20:46 -0300

    imc

--- imc/js/main.js
+++ imc/js/main.js
@@ -1,63 +1,73 @@
-const calcularImc= () => {
-    const nome = document.getElementById("nome").value;
-    const altura = document.getElementById("altura").value;
-    const peso = document.getElementById("peso").value;
-    const resultado = document.getElementById("resultado"). value;
-    if(nome !=='' && peso !=='' && altura !==''){
-const imc = (peso/(altura*altura));
-console.log('altura: ', altura);
+// outra forma de escrever uma funcao
+// function limparCampos(){
+//      codigo here
+// }
+const calcularImc = () => {
+    const nome = 
+        document.getElementById("nome").value;
+    const altura = 
+        document.getElementById("altura").value;
+    const peso = 
+        document.getElementById("peso").value;
+    const resultado = 
+        document.getElementById("resultado");
 
-let classificacao = '';
+    if( nome !== '' && peso !== '' && altura !== ''){
+        const imc = (peso/(altura*altura));
+        //comando para exibir no console 'f12'
+        console.log('altura: ', altura);
 
-if(imc < 18.5){
-    classificacao ='Abaixo do peso';
+        let classificacao = '';
 
-}
-else if (imc < 25){
-    classificacao = 'peso ideal'
-}
+        if(imc < 18.5){
+            classificacao = 'Abaixo do peso';
+        }
+        else if(imc < 25){
+            classificacao = 'compeso ideal. parabens!!!';
+        }
+        else if (imc < 30){
+            classificacao = 'levemente acima do peso.';
+        }
+        else if (imc < 35){
+            classificacao = 'obesidade grau I';
+        }
+        else if(imc < 40){
+            classificacao = 'obesidade grau II';
+        }
+        else {
+            classificacao = 'obesidade grau III. cuidado!!!';
+        }
+        resultado.textContent = 
+            `${nome}, seu IMC e ${imc} e vc esta ${classificacao}`
 
-else if (imc < 30){
-    classificacao = 'levemente acima do peso'
-}
-
-else if (imc < 35){
-    classificacao = 'obesidade grau 1'
-}
-
-else if (imc < 40){
-    classificacao = 'obesidade grau 2'
-}
-
-else{
-    classificacao = 'obesidade grau 3, cuidado!'
-}
-resultado.textContent = `${nome}, seu IMC e ${imc} e vc esta ${classificacao}`
-}
-
-     else {
-         resultado.textContent =
-         'preencha todos os campos'
-     }
+    }
+    else {
+        resultado.textContent = 
+        'preencha todos os campos';
+    }
+    
 };
 
-function limparCampos(){
-    const nome = document.getElementById("nome").value = null;
-    const altura = document.getElementById("altura").value = null;
-    const peso = document.getElementById("peso").value = null;
-    
-
-
-}
+const limparCampos = () => {
+    const nome = 
+        document.getElementById("nome").value = null;
+    const altura = 
+        document.getElementById("altura").value = null;
+    const peso = 
+        document.getElementById("peso").value = null;
+};
 
 const mascaraAltura = (value, pattern) => {
-    let i = 0
+    let i = 0;
     let v = value.tostring();
     v = v.replace(/\D/g, '');
     return pattern.replace(/#/g, () => v[i++] || '');
 };
 
-const aplicar(value){
-    const formatado = mascaraAltura(value, '#,##');
-    document.getElementById('altura'). value = formatado;
+const aplicar = (value) => {
+    const formatado = mascaraAltura(value, '#.##');
+    document.getElementById('altura').value = formatado;
 }
+
+  
+
