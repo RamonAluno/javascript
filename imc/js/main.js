157044118a02d79a51961d4257ffbe8bb194dffb
Rz <ramon.o.azevedo@aluno.senai.br>
2023-12-05 19:25:46 -0300

    imc

--- imc/js/main.js
+++ imc/js/main.js
@@ -1,65 +1,44 @@
-// outra forma de escrever uma funcao
-// function limparCampos(){
-//      codigo here
-// }
 const calcularImc = () => {
-    const nome = 
-        document.getElementById("nome").value;
-    const altura = 
-        document.getElementById("altura").value;
-    const peso = 
-        document.getElementById("peso").value;
-    const resultado = 
-        document.getElementById("resultado");
+    const nome = document.getElementById("nome").value;
+    const altura = document.getElementById("altura").value;
+    const peso = document.getElementById("peso").value;
+    const resultado = document.getElementById("resultado");
 
-    if( nome !== '' && peso !== '' && altura !== ''){
-        const imc = (peso/(altura*altura));
-        //comando para exibir no console 'f12'
-        console.log('altura: ', altura);
+    if (nome !== '' && peso !== '' && altura !== '') {
+        const imc = (peso / (altura * altura)).toFixed(2);
 
         let classificacao = '';
 
-        if(imc < 18.5){
+        if (imc < 18.5) {
             classificacao = 'Abaixo do peso';
+        } else if (imc < 25) {
+            classificacao = 'Peso ideal';
+        } else if (imc < 30) {
+            classificacao = 'Levemente acima do peso';
+        } else if (imc < 35) {
+            classificacao = 'Obesidade grau 1';
+        } else if (imc < 40) {
+            classificacao = 'Obesidade grau 2';
+        } else {
+            classificacao = 'Obesidade grau 3, cuidado!';
         }
-        else if(imc < 25){
-            classificacao = 'compeso ideal. parabens!!!';
-        }
-        else if (imc < 30){
-            classificacao = 'levemente acima do peso.';
-        }
-        else if (imc < 35){
-            classificacao = 'obesidade grau I';
-        }
-        else if(imc < 40){
-            classificacao = 'obesidade grau II';
-        }
-        else {
-            classificacao = 'obesidade grau III. cuidado!!!';
-        }
-        resultado.textContent = 
-            `${nome}, seu IMC e ${imc} e vc esta ${classificacao}`
 
+        resultado.textContent = `${nome}, seu IMC é ${imc} e você está ${classificacao}`;
+    } else {
+        resultado.textContent = 'Preencha todos os campos';
     }
-    else {
-        resultado.textContent = 
-        'preencha todos os campos';
-    }
-    
+    limparCampos();
 };
 
-const limparCampos = () => {
-    const nome = 
-        document.getElementById("nome").value = null;
-    const altura = 
-        document.getElementById("altura").value = null;
-    const peso = 
-        document.getElementById("peso").value = null;
-};
+function limparCampos() {
+    document.getElementById("nome").value = '';
+    document.getElementById("altura").value = '';
+    document.getElementById("peso").value = '';
+}
 
 const mascaraAltura = (value, pattern) => {
     let i = 0;
-    let v = value.tostring();
+    let v = value.toString();
     v = v.replace(/\D/g, '');
     return pattern.replace(/#/g, () => v[i++] || '');
 };
@@ -68,6 +47,3 @@ const aplicar = (value) => {
     const formatado = mascaraAltura(value, '#.##');
     document.getElementById('altura').value = formatado;
 }
-
-  
-
